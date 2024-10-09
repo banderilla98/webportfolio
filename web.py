de4d2b384a75bbbbf019633d192b60036c7c7c9d
banderilla98 <zahidangeles11@gmail.com>
2024-10-09 15:59:12 -0700

    starting

---
+++ web.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+
+web = Flask(__name__)
+@web.route('/')
+def home():
+   return render_template('home.html')
+
+
+@web.route('/about/')
+def about():
+   return render_template('about.html')
+
+
+if __name__ == '__main__':
+   web.run(debug=True)
